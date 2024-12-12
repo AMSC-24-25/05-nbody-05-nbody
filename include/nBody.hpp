40d911c5a16cc72392bade41388bb77e4a110f77
Daniele Piano <dpiano01@gmail.com>
2024-12-12 22:55:11 +0100

    nBody.hpp :: adding header-guards, G constant as constexpr, floating_point-templated values

--- include/nBody.hpp
+++ include/nBody.hpp
@@ -1,31 +1,40 @@
+#ifndef TEAM_05_NBODY_NBODY_H
+#define TEAM_05_NBODY_NBODY_H
+
 #include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <concepts>
 
+template<std::floating_point FP>
 struct Particle {
-    const double mass;
-    std::vector<double> pos;
-    std::vector<double> vel;
+    const FP mass;
+    std::vector<FP> pos;
+    std::vector<FP> vel;
 };
 
-template <typename T>
+template<std::floating_point FP>
 class AbstractNbody {
 public:
-    virtual ~AbstractNbody() {}
+    virtual ~AbstractNbody() = default;
 
-    virtual void setup (unsigned int N) = 0;
+    virtual void setup(unsigned int N) = 0;
 
-    virtual void solve () = 0;
+    virtual void solve() = 0;
 
-    virtual void output () = 0;
+    virtual void output() = 0;
 
 protected:
-    unsigned int const N;
-    unsigned double const delta_t;
-    std::vector<T> particles;
-    std::vector<double> forces_x;
-    std::vector<double> forces_y;
-    std::vector<double> time;
+    static constexpr FP G = 6.673e-11;
+
+    const unsigned int N;
+    const FP delta_t;
+    std::vector<Particle<FP>> particles;
+    std::vector<FP> forces_x;
+    std::vector<FP> forces_y;
+    std::vector<FP> time;
 };
+
+#endif //TEAM_05_NBODY_NBODY_H
