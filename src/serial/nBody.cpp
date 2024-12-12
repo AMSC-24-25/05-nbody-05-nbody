aa5418ba1323cacf65c109dd62aa868904399bd9
Daniele Piano <dpiano01@gmail.com>
2024-12-12 22:58:24 +0100

    nBody.cpp :: alignment to abstract class

--- src/serial/nBody.cpp
+++ src/serial/nBody.cpp
@@ -1,49 +1,52 @@
-#include "../../include/nBody.hpp"
+#include "nBody.hpp"
+#include <concepts>
 
-class Nbody : public AbstractNbody<Particle> {
+template<std::floating_point FP>
+class Nbody : public AbstractNbody<FP> {
 public:
-    void setup (unsigned int N) override {
+    void setup(unsigned int N) override {
         // da dove prendiamo i dati delle particelle (masse, posizioni iniziali, velocità iniziali)?
     }
 
-    void solve () override {
-        const double G = -6.673e-11;
-        for (unsigned int i = 0; i < time.size(); i++) {
-            for (unsigned int q = 0; q < N; q++)
-            {
-                for (unsigned int k = q + 1; k < N; k++)
-                {
-                    double x_diff = particles[q].pos[0] - particles[k].pos[0];
-                    double y_diff = particles[q].pos[1] - particles[k].pos[1];
-                    double dist = sqrt(x_diff * x_diff + y_diff * y_diff);
-                    double dist_cube = dist * dist * dist;
+    void solve() override {
+        using AbstractNbody<FP>::G;
 
-                    double force_qk_x = G * particles[q].mass * particles[k].mass / dist_cube * x_diff;
-                    double force_qk_y = G * particles[q].mass * particles[k].mass / dist_cube * y_diff;
+        for (unsigned int i = 0; i < this->time.size(); i++) {
+            for (unsigned int q = 0; q < this->N; q++) {
+                for (unsigned int k = q + 1; k < this->N; k++) {
+                    FP x_diff = this->particles[q].pos[0] - this->particles[k].pos[0];
+                    FP y_diff = this->particles[q].pos[1] - this->particles[k].pos[1];
+                    FP dist = sqrt(x_diff * x_diff + y_diff * y_diff);
+                    FP dist_cube = dist * dist * dist;
 
-                    forces_x[q] += force_qk_x;
-                    forces_x[k] -= force_qk_x;
-                    forces_y[q] += force_qk_y;
-                    forces_y[k] -= force_qk_y;
+                    FP force_qk_x = G * this->particles[q].mass * this->particles[k].mass / dist_cube * x_diff;
+                    FP force_qk_y = G * this->particles[q].mass * this->particles[k].mass / dist_cube * y_diff;
+
+                    this->forces_x[q] += force_qk_x;
+                    this->forces_x[k] -= force_qk_x;
+                    this->forces_y[q] += force_qk_y;
+                    this->forces_y[k] -= force_qk_y;
                 }
             }
 
-            for (unsigned int j = 0; j < N; j++) {
-                particles[j].pos[0] = particles[j].pos[0] + delta_t * particles[j].vel[0];
-                particles[j].pos[1] = particles[j].pos[1] + delta_t * particles[j].vel[1];
+            for (unsigned int j = 0; j < this->N; j++) {
+                this->particles[j].pos[0] = this->particles[j].pos[0] + this->delta_t * this->particles[j].vel[0];
+                this->particles[j].pos[1] = this->particles[j].pos[1] + this->delta_t * this->particles[j].vel[1];
 
-                particles[j].vel[0] = particles[j].vel[0] + delta_t * forces_x[j] / particles[j].mass;
-                particles[j].vel[1] = particles[j].vel[1] + delta_t * forces_y[j] / particles[j].mass;
+                this->particles[j].vel[0] =
+                        this->particles[j].vel[0] + this->delta_t * this->forces_x[j] / this->particles[j].mass;
+                this->particles[j].vel[1] =
+                        this->particles[j].vel[1] + this->delta_t * this->forces_y[j] / this->particles[j].mass;
 
                 // output
             }
 
         }
-        
-        
+
+
     }
 
-    void output () override {
+    void output() override {
 
     }
 };
