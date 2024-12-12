43479e6ca2ca8af2ac5fb474a0eb7ed8e1d0c4e1
StePedre <stefanopedretti60@gmail.com>
2024-12-12 10:02:47 +0000

    test

---
+++ Serial/src/main.cpp
@@ -0,0 +1,19 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct {
+    double mass,
+    vector<double> pos,
+    vector<double> vel,
+    vector<double> acc,    
+    double force,
+} particle;
+
+int
+main(int argc, char * argv[]) {
+    
+
+}
